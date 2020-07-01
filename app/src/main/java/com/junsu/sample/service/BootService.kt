4dfe3336b54e2f572ed8cf63ff9463412067c339
이준수 <jslee@mtcom.co.kr>
2020-07-01 10:20:58 +0900

    IntentService -> JobIntentService

--- app/src/main/java/com/junsu/sample/service/BootService.kt
+++ app/src/main/java/com/junsu/sample/service/BootService.kt
@@ -1,23 +1,15 @@
 package com.junsu.sample.service
 
-import android.app.*
 import android.content.Context
 import android.content.Intent
-import android.graphics.Color
 import android.os.Build
-import androidx.annotation.RequiresApi
-import androidx.core.app.NotificationCompat
-import com.junsu.sample.R
+import androidx.core.app.JobIntentService
 import timber.log.Timber
 
 
-class BootService : IntentService(TAG) {
+class BootService : JobIntentService() {
     companion object {
-        private const val TAG = "BootService"
         private const val ACTION_BOOT_SERVICE = "com.junsu.sample.service.actionBootService"
-        private const val NOTIFICATION_CHANNEL_ID = "com.junsu.sample"
-        private const val NOTIFICATION_CHANNEL_NAME = "BootService"
-        private const val SERVICE_CHANNEL_ID = 3000
         fun start(context: Context) {
             Intent(context, BootService::class.java).also { intent ->
                 intent.action = ACTION_BOOT_SERVICE
@@ -30,46 +22,8 @@ class BootService : IntentService(TAG) {
         }
     }
 
-    override fun onCreate() {
-        makeNotification()
-        super.onCreate()
-    }
-
-    @RequiresApi(Build.VERSION_CODES.O)
-    private fun createNotificationChannel(
-        channelId: String,
-        channelName: String,
-        f: (NotificationChannel) -> Unit = {}
-    ): String {
-        val channel =
-            NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_NONE)
-        f(channel)
-        val notificationManager =
-            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        notificationManager.createNotificationChannel(channel)
-        return channelId
-    }
-
-    private fun makeNotification() {
-        val channelId =
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                createNotificationChannel(NOTIFICATION_CHANNEL_ID, NOTIFICATION_CHANNEL_NAME)
-            } else {
-                ""
-            }
-
-        val builder = NotificationCompat.Builder(this, channelId)
-            .setContentTitle(getString(R.string.app_name))
-            .setContentText("running...")
-            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-            .setAutoCancel(true)
-        val notification = builder.build()
-
-        startForeground(SERVICE_CHANNEL_ID, notification)
-    }
-
-    override fun onHandleIntent(intent: Intent?) {
-        Timber.d("${intent?.action}")
+    override fun onHandleWork(intent: Intent) {
+        Timber.d("${intent.action}")
         when (intent?.action) {
             ACTION_BOOT_SERVICE -> {
                 Thread.sleep(5000)
